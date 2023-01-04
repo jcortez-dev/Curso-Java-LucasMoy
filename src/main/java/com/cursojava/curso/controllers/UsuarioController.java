b6a1eb5ed341a9b2331b0beff203b9250f02f590
Jorge Cortez <j.cortez01@ufromail.cl>
2023-01-04 16:28:18 -0300

    Se agregan funcionalidades crud

---
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -0,0 +1,70 @@
+package com.cursojava.curso.controllers;
+
+import com.cursojava.curso.dao.UsuarioDao;
+import com.cursojava.curso.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+
+    @RequestMapping(value = "api/usuario/{id}")
+    public Usuario getUsuario (@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("121231");
+        return usuario;
+    }
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+    public List<Usuario> getUsuarios (){
+        return usuarioDao.getUsuarios();
+    }
+
+
+    @RequestMapping(value = "api/editar")
+    public Usuario editar (){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("121231");
+        return usuario;
+    }
+    @RequestMapping(value = "api/eliminar/{id}", method = RequestMethod.DELETE)
+    public void eliminar (@PathVariable Long id){
+        usuarioDao.eliminar(id);
+    }
+    @RequestMapping(value = "api/buscar")
+    public Usuario buscar (){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("121231");
+        return usuario;
+    }
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
+    public void registrarUsuario (@RequestBody Usuario usuario){
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1, 1024, 1, usuario.getPassword());
+
+        usuario.setPassword(hash);
+
+        usuarioDao.registrar(usuario);
+    }
+
+}
---
+++ src/main/resources/static/js/usuarios.js
@@ -0,0 +1,48 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  cargarUsuarios();
+
+  $('#usuarios').DataTable();
+});
+
+
+async function cargarUsuarios(){
+  const request = await fetch("api/usuarios", {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+  const usuarios = await request.json();
+
+
+
+  let listadoHtml = '';
+  for (let usuario of usuarios){
+    let botonEliminar = '<a href="#" onclick="eliminarUsuario('+usuario.id+')" class="btn btn-danger btclassNamecle btn-sm"> <i class="fas fa-trash"></iclassName </a>'
+    let usuarioHtml = '<tr> <td>'+usuario.id+'</td> <td>'+usuario.nombre+' '+usuario.apellido+'</td> <td>'+usuario.email+'</td> <td>'+usuario.telefono+'</td> <td>'+botonEliminar+'</td> </tr>';
+    listadoHtml+=usuarioHtml;
+  }
+
+
+  document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+
+}
+
+async function eliminarUsuario(id){
+
+  if(!confirm("¿Desea eliminar este usuario?")){
+    return;
+  }
+
+  const request = await fetch("api/eliminar/"+id, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+
+  location.reload();
+}
